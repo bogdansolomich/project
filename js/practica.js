971d4516e9fd5231f79aeff4135096a385ad7e71
Bogdan <bogdansolomich@gmail.com>
2022-01-24 17:41:10 +0200

    practica.js mod

--- js/practica.js
+++ js/practica.js
@@ -1,1 +1,21 @@
-alert("тесатим файл");
+'use strict';
+
+const numberOfffiles = +prompt ("Сколько фильмов вы уже смотрели?", "");
+
+const personalMovie08 = {
+count: numberOfffiles,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+b = prompt("На сколько оцените его?", ""),
+z = prompt("Один из последних просмотренных фильмов", ""),
+d = prompt("На сколько оцените его?", "");
+
+personalMovie08.movies[a] = b;
+personalMovie08.movies[z] = d;
+
+console.log(personalMovie08);
